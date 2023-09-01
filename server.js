e432d907fe6729f94e4f62349d8147e0d6c69e7e
kosekijsx <leandrobordondev@gmail.com>
2023-08-31 21:56:02 -0300

    🐳 Render config️

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const mongoose = require("mongoose");
 const app = require("./src/app");
 const routes = require("./src/routes/routes");
 const cookieParser = require("cookie-parser");
+const { error } = require("console");
 const PORT = process.env.PORT || 3000; // default port
 // Mongoose configuration
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.URL_DB)
   .then(() => console.log("--- ✅ Database connection established ---"))
-  .catch(() => console.log("--- ❌ Database connection rejected ---"));
+  .catch((error) =>
+    console.log("--- ❌ Database connection rejected ---", error)
+  );
 
 // VIEW SETTINGS
 app.set("views", path.join(__dirname, "./src/public/views")); // use pug templates file in /public/views subdirectory
