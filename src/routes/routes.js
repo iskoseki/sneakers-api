f053fafc9180a01fdf98c60c6599cfe113619a3d
kosekijsx <leandrobordondev@gmail.com>
2023-09-23 14:39:51 -0300

    Home update v0.1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,59 +1,60 @@
-const express = require('express')
-const Product = require('../models/productModel')
-const router = express.Router()
+const express = require("express");
+const Product = require("../models/productModel");
+const router = express.Router();
 // 👇️ handle uncaught exceptions
-process.on('uncaughtException', function (err) {
-  console.log(err)
-})
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
 
 // 👷🏼‍♀️ GET /home
-router.get('/', function (req, res, err) {
-  res.render('index', { title: 'pug-Bootstrap' })
-})
+router.get("/", function (req, res, err) {
+  res.render("index", { title: "pug-Bootstrap" });
+});
 
 // ✅ GET method /products
-router.get('/products', async (req, res, err) => {
+router.get("/products", async (req, res, err) => {
   try {
-    const data = await Product.find()
-    res.json(data)
-    console.log('Product req created: ', data)
+    const data = await Product.find();
+    res.json(data);
+    console.log("Product req created: ", data);
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({ message: error.message })
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 // ✅ GET /products/:id
-router.get('/products/:id', async (req, res, err) => {
+router.get("/products/:id", async (req, res, err) => {
   try {
-    const ID = req.params.id
-    const data = await Product.findById(ID)
-    res.json(data)
+    const ID = req.params.id;
+    const data = await Product.findById(ID);
+    res.json(data);
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-})
+});
+
 // ✅ POST /products
-router.post('/products', async (req, res, err) => {
+router.post("/products", async (req, res, err) => {
   try {
-    const product = await Product.create(req.body)
-    res.status(200).json(product)
-    console.log('Product req created: ', product)
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+    console.log("Product req created: ", product);
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({ message: error.message })
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 // ✅ DELETE by ID Method
-router.delete('/products/:id', async (req, res, err) => {
+router.delete("/products/:id", async (req, res, err) => {
   try {
-    const ID = req.params.id
-    const data = await Product.findByIdAndDelete(ID)
-    res.json({ message: `The sneaker: ${data.name} has been deleted..` })
+    const ID = req.params.id;
+    const data = await Product.findByIdAndDelete(ID);
+    res.json({ message: `The sneaker: ${data.name} has been deleted..` });
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
